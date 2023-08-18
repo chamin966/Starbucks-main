f5aeb1d38cace994c2d9bb8ae7bf5c007a226cd1
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-18 18:28:11 +0900

    Chore: 초기 환경 세팅
      "dependencies": {
        "@types/node": "20.5.0",
        "@types/react": "18.2.20",
        "@types/react-dom": "18.2.7",
        "eslint": "8.47.0",
        "eslint-config-next": "13.4.18",
        "next": "13.4.18",
        "next-seo": "^6.1.0",
        "react": "18.2.0",
        "react-dom": "18.2.0",
        "sass": "^1.66.0",
        "scss-reset": "^1.4.2",
        "typescript": "5.1.6"
      },
      "devDependencies": {
        "eslint-config-prettier": "^9.0.0",
        "prettier": "3.0.2"
      }

---
+++ my-starbucks-main/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <div>스타벅스 메인 화면 클론 프로젝트</div>
+    </>
+  );
+}
