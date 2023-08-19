8390ad98696877d0858b11022b0104066d1de3e2
Minchan <98478661+chamin966@users.noreply.github.com>
2023-08-19 15:02:49 +0900

    Design: 헤더 컴포넌트 UI 제작

--- my-starbucks-main/pages/index.tsx
+++ my-starbucks-main/pages/index.tsx
@@ -1,11 +1,21 @@
+import Header from '@/components/home/Header';
 import Head from 'next/head';
+import { Arimo } from 'next/font/google';
+
+const globalFont = Arimo({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+});
+
 export default function Home() {
   return (
-    <>
+    //헤더 position: fixed 테스트용 높이
+    <div className={globalFont.className} style={{ height: '200vh' }}>
       <Head>
         <title>Create Next App</title>
       </Head>
+      <Header />
       <div>스타벅스 메인 화면 클론 프로젝트</div>
-    </>
+    </div>
   );
 }
